fa95da88db0f4f57e33b831ce7575c0401932861
vd <vd@norse.digital>
2016-05-24 10:17:23 +0300

    Made the product page
    Made the aside part of the  order page

--- src/javascripts/productSlider.js
+++ src/javascripts/productSlider.js
@@ -13,12 +13,34 @@ $(document).ready(function() {
 
     $('.slider__nav').slick({
         slidesToShow: 4,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
         asNavFor: '.slider__for',
         dots: false,
         arrows: true,
-        //centerMode: true,
         adaptiveHeight: true
     });
 
+
+});
+
+$(document).ready(function () {
+    $('.color__chosen-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        speed: 500,
+        centerMode: true,
+        variableWidth: true,
+        arrows: true,
+        infinite: true
+    });
+    //$('.color__chosen-slider').on('click', '.slick-slide', callBack);
+    //$('.slider__img-item').on('click', function(){
+    //    //var $this = $(this);
+    //    //var $parentslider = $this.closest('.color__chosen-slider');
+    //    //var $slide = $parentslider.find('.slider__img-item');
+    //    //
+    //    //$slide.toggleClass("slick-current");
+    //    var currentSlide = $('.slider__img-item').slick('slick-current');
+    //});
 });
