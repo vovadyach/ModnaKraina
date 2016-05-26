628334f194ad4ab47fa5f7a8444883a880af854f
vd <vd@norse.digital>
2016-05-26 18:27:50 +0300

    Added new modification

--- src/javascripts/customSlider.js
+++ src/javascripts/customSlider.js
@@ -9,6 +9,8 @@
     var $prevSlide = $('.prev__slide');
     var $nextSlide = $('.next__slide');
 
+    var $btnsSlide = $().add( $prevSlide).add( $nextSlide );
+
     var position = 0;
     var lastPosition = 0;
 
@@ -17,6 +19,50 @@
     $sliderCustom.width(containerWidth * $sliderItem.length);
     $sliderItem.width(containerWidth);
 
+    window.slideTo = function (pos) {
+        if ( pos < 0 ) pos = 0;
+        if ( pos >= $sliderItem.length ) pos = $sliderItem.length - 1;
+
+        $sliderCustom.css('left', -pos * containerWidth );
+    };
+
+    $btnsSlide.on('click', function (event) {
+        var $this = $(this);
+
+        if ( $this.hasClass('next__slide') ) {
+            position++;
+        } else {
+            position--;
+        }
+
+        if ( position < 0 ) {
+            position = $sliderItem.length - 1;
+        }
+
+        if ( position >= $sliderItem.length ) {
+            position = 0;
+        }
+
+        console.log( position );
+
+        slideTo(position);
+
+        event.preventDefault();
+    });
+
+
+    //$nextSlide.on('click', function () {
+    //    position++;
+    //    var shift;
+    //    if (position <= $sliderItem.length) {
+    //        shift = -position * containerWidth;
+    //        $sliderCustom.css("left", shift);
+    //        if (position > $sliderItem.length) {
+    //            position = 0;
+    //        }
+    //
+    //    }
+    //});
 
     var shift = -position * containerWidth;
     $sliderCustom.css("left", shift);
